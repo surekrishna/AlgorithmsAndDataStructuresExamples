f149b55800999e1fea17d99c709c6fd45f3f24c3
Krishna <surekrrishna@gmail.com>
2018-11-11 22:36:52 +0530

    created linear search example

---
+++ src/main/java/com/krish/algorithms/and/datastructures/LinearSerachExampleOne.java
@@ -0,0 +1,31 @@
+package com.krish.algorithms.and.datastructures;
+
+/**
+ * @author Krishna Sure
+ * Java code for linearly search x in arr[].  If x  
+ * is present  then return its  location,  otherwise 
+ * return -1 
+ */
+public class LinearSerachExampleOne {
+
+	public static void main(String[] args) {
+		//array of n numbers
+		int[] randomNumbers = new int[] {1,3,6,78,34,87,23,781,356,991,234,12,56,72,521};
+		int x = 234;		
+		int index = getIndexOfNumber(randomNumbers, x);
+		System.out.println("Index of Random Number {} "+index);
+	}
+	
+	//This method returns the index of x in randomNumbers[]
+	private static int getIndexOfNumber(int[] randomNumbers, int x) {
+		for(int i = 0; i < randomNumbers.length; i ++) {
+			//Returns index if the element is found
+			if(randomNumbers[i] == x) {
+				return i;
+			}			
+		}
+		//Returns -1 if the element is not found
+		return -1;
+	}
+
+}
--- src/main/java/com/krish/algorithms/and/datastructures/main/AlgorithmsAndDataStructuresExamplesApplication.java
+++ src/main/java/com/krish/algorithms/and/datastructures/main/AlgorithmsAndDataStructuresExamplesApplication.java
@@ -1,4 +1,4 @@
-package com.krish.algorithms.and.datastructures.AlgorithmsAndDataStructuresExamples;
+package com.krish.algorithms.and.datastructures.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
