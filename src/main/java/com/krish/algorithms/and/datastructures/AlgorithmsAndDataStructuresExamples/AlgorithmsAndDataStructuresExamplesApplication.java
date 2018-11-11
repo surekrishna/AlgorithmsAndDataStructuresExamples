e1ff02801cd3455acc6d6ffa46f8a627d160e0cc
Krishna <surekrrishna@gmail.com>
2018-11-11 21:49:14 +0530

    created new spring boot application for algorithms and datastructures examples

---
+++ src/main/java/com/krish/algorithms/and/datastructures/AlgorithmsAndDataStructuresExamples/AlgorithmsAndDataStructuresExamplesApplication.java
@@ -0,0 +1,12 @@
+package com.krish.algorithms.and.datastructures.AlgorithmsAndDataStructuresExamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsAndDataStructuresExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmsAndDataStructuresExamplesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/krish/algorithms/and/datastructures/AlgorithmsAndDataStructuresExamples/AlgorithmsAndDataStructuresExamplesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.krish.algorithms.and.datastructures.AlgorithmsAndDataStructuresExamples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlgorithmsAndDataStructuresExamplesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
